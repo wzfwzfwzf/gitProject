e2f0b157a041f4f143238810b5d19d549233db61
wzf <1454746033@qq.com>
2019-12-18 09:23:18 +0800

    666

--- github/src/main/java/com/wzf/github/GithubApplication.java
+++ github/src/main/java/com/wzf/github/GithubApplication.java
@@ -9,6 +9,7 @@ public class GithubApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubApplication.class, args);
         System.out.println("1");
+        System.out.println("666");
     }
 
 }
